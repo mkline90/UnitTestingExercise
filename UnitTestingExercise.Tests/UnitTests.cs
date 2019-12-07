f9c0b528e58b7372062ca9e583e67b1b209beabe
Michael Kline <mikeymaederk@gmail.com>
2019-12-06 18:42:01 -0600

    created methods and unit tests

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,111 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(10, 7, 3, 20)]
+        [InlineData(100, 75, 25, 200)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(-5, 5, -10, -10)]
+        [InlineData(-10, -7, -17, -34)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            int actual = challenger.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(10, 7, 3)]
+        [InlineData(10, 15, -5)]
+        [InlineData(0, 7, -7)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-5, 5, -10)]
+        [InlineData(100, 25, 75)]
+        public void Subtract(int minuend, int subtrahend, int expected)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            double actual = challenger.Subtract(minuend, subtrahend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
-        public void Multiply(int num1, int num2, int expected)
+        [InlineData(10, 7, 70)]
+        [InlineData(100, 75, 7500)]
+        [InlineData(1, 1, 1)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-5, 5, -25)]
+        public void Multiply(long factor1, long factor2, long product)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            long actual = challenger.Multiply(factor1, factor2);
             //Assert
-
+            Assert.Equal(product, actual);
         }
 
         [Theory]
-        [InlineData()]
-        public void Divide(int num1, int num2, int expected)
+        [InlineData(10, 5, 2)]
+        [InlineData(25, 5, 5)]
+        [InlineData(3.14, -1.72, -1.826)]
+        [InlineData(10, 0, 0)]
+        [InlineData(-100, -10, 10)]
+        public void Divide(double dividend, double divisor, double quotient)
         {
             //Arrange
-
+            UnitTestMethods challenger = new UnitTestMethods();
             //Act
-
+            double actual = challenger.Divide(dividend, divisor);
             //Assert
-
+            Assert.Equal(quotient, actual);
         }
 
-        [Fact]
-        public void YourMethodName()
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("apple", false)]
+        [InlineData("banana", false)]
+        [InlineData("orange", false)]
+
+        public void IsStringAPalindrome(string input, bool expected)
         {
-            //Arrange
+            // Arrange
+            UnitTestMethods challenger = new UnitTestMethods();
 
-            //Act
+            // Act
+            bool actual = challenger.IsStringAPalindrome(input);
 
-            //Assert
-
+            // Assert
+            Assert.Equal(actual, expected);
         }
 
-        [Fact]
-        public void YourMethodName2()
+        [Theory]
+        [InlineData("Mike", true)]
+        [InlineData("Michael", true)]
+        [InlineData("Mikey", true)]
+        [InlineData("mike", true)]
+        [InlineData("mikey", true)]
+        [InlineData("stan", false)]
+        [InlineData("Jeff", false)]
+        [InlineData("michelle", false)]
+        [InlineData("jessica", false)]
+
+        public void IsYourNameMyName(string name, bool expected)
         {
-            //Arrange
+            // Arrange
+            UnitTestMethods challenger = new UnitTestMethods();
 
-            //Act
+            // Act
+            bool actual = challenger.IsYourNameMyName(name);
 
-            //Assert
+            // Assert
+            Assert.Equal(actual, expected);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,23 +4,53 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Right-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
 
+        public int Add(int num1, int num2, int num3)
+        {
+            throw new NotImplementedException();
+        }
+        
+
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
+
+        public double Subtract(double minuend, double subtrahend)
+        {
+            throw new NotImplementedException();
+        }
 
         // Create a Multiply method that passes 2 integers
 
+        public long Multiply(long factor1, long factor2)
+        {
+            throw new NotImplementedException();
+        }
+
         // Create a Divide method that passes 2 integers
 
-
+        public double Divide(double dividend, double divisor)
+        {
+            throw new NotImplementedException();
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+        public bool IsStringAPalindrome(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsYourNameMyName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
     }
 }
